502ac18a92f4029b2a3092d2f534df7c10b4a227
Edi Buhic <edi@buhic.se>
2024-10-14 14:49:29 +0200

    Lab 7

---
+++ lab7/src/lab7/Cat.java
@@ -0,0 +1,33 @@
+package lab7;
+
+public class Cat extends Pet {
+
+	private boolean indoor;
+	
+	public Cat(String name) {
+		super(name);
+		this.indoor = false;
+	}
+	
+	public Cat(String name, int age) {
+		super(name, age);
+		this.indoor = false;
+	}
+	
+	public Cat(String name, int age, boolean isIndoor) {
+		this(name, age);
+		this.indoor = isIndoor;
+	}
+	
+	public String makeSound() {
+		return "Mjau, mjau";
+	}
+	
+	public String getDescription() {
+		return "Pet: Cat, Name: " + super.getName() + ", Age: " + super.getAge() + ".";
+	}
+	
+	public Boolean isIndoor() {
+		return indoor;
+	}
+}
---
+++ lab7/src/lab7/Dog.java
@@ -0,0 +1,33 @@
+package lab7;
+
+public class Dog extends Pet {
+
+	private boolean hunting;
+	
+	public Dog(String name) {
+		super(name);
+		this.hunting = false;
+	}
+	
+	public Dog(String name, int age) {
+		super(name, age);
+		this.hunting = false;
+	}
+	
+	public Dog(String name, int age, boolean isIndoor) {
+		this(name, age);
+		this.hunting = isIndoor;
+	}
+	
+	public String makeSound() {
+		return "Mjau, mjau";
+	}
+	
+	public String getDescription() {
+		return "Pet: Cat, Name: " + super.getName() + ", Age: " + super.getAge() + ".";
+	}
+	
+	public Boolean isHunting() {
+		return hunting;
+	}
+}
