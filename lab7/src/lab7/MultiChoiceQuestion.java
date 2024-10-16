9a8dc95545cbf3e77a878474e97c6043408ec4cf
Edi Buhic <edi@buhic.se>
2024-10-16 17:47:16 +0200

    Lab 7

--- lab7/src/lab7/MultiChoiceQuestion.java
+++ lab7/src/lab7/MultiChoiceQuestion.java
@@ -4,7 +4,7 @@ public class MultiChoiceQuestion extends Question {
 
 	String[] alternatives = new String[11];
 	Boolean[] isTrue = new Boolean[11];
-	int totalAlternatives = 1;
+	int totalAlternatives = 0;
 	
 	
 	public void addChoice(String alt, boolean isTrue) {
